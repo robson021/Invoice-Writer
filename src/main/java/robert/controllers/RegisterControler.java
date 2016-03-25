dd4ad9280a65cdc172cfdd3a8ff5ab929fa26dbe
robert <nowak021@gmail.com>
2016-03-25 20:31:26 +0100

    commint

--- src/main/java/robert/controllers/RegisterControler.java
+++ src/main/java/robert/controllers/RegisterControler.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class RegisterControler {
 
-    @RequestMapping("/register")
+    @RequestMapping(value = {"/register"})
     String goRegister() {
         return "register";
     }
