75eb18c5daad5f414d1570f1adce17383e74f530
robert <nowak021@gmail.com>
2016-03-25 16:55:25 +0100

    commint

---
+++ src/main/java/robert/InvoiceWriterApplication.java
@@ -0,0 +1,12 @@
+package robert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoiceWriterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InvoiceWriterApplication.class, args);
+    }
+}
