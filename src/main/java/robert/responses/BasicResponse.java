9a2006b538edbfd40300a8841f060e6d7cb0ff34
robert <nowak021@gmail.com>
2016-03-26 22:11:26 +0100

    commit

---
+++ src/main/java/robert/responses/BasicResponse.java
@@ -0,0 +1,16 @@
+package robert.responses;
+
+/**
+ * Created by robert on 26.03.16.
+ */
+public class BasicResponse {
+    private boolean result = false;
+
+    public boolean isResult() {
+        return result;
+    }
+
+    public void setResult(boolean result) {
+        this.result = result;
+    }
+}
