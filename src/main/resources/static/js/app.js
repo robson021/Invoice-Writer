ecf5b85368975343c13039e270cc6f76eb638968
robert <nowak021@gmail.com>
2016-05-03 17:08:28 +0200

    todo: fix ui-route-provider

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,1 +1,21 @@
 angular.module("ngApp", ['ngMaterial'])
+
+
+// adding ui.router disables all the view
+/*
+ angular.module("ngApp", ['ngMaterial', 'ui.router'])
+ .config(function ($stateProvider) {
+ $stateProvider
+ .state('first', {
+ url: '/login-register',
+ template: '<h4>Test state 1</h4>'
+ })
+ .state('secound', {
+ url: '/test',
+ template: '<h4>Test state 2</h4>'
+ })
+
+
+
+ })
+ */
