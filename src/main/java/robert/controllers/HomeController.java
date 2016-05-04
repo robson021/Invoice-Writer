d093262887cc84301c0760528001a22b3fbbf153
robert <nowak021@gmail.com>
2016-05-04 11:19:15 +0200

    thymeleaf removed

--- src/main/java/robert/controllers/HomeController.java
+++ src/main/java/robert/controllers/HomeController.java
@@ -1,17 +1,14 @@
 package robert.controllers;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-
 /**
  * Created by robert on 25.03.16.
  */
 
-@Controller
+//@Controller
 public class HomeController {
 
-    @RequestMapping(value = {"/", "/home", "/start", "/index"})
+    /*@RequestMapping(value = {"/", "/home", "/start", "/index"})
     public String goHome() {
         return "home";
-    }
+    }*/
 }
