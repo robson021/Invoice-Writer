6f3d014c664efd6cd57f0619c263ef65f434dee0
robert <nowak021@gmail.com>
2016-05-23 00:00:54 +0200

    h2 console enabled agin

--- src/main/java/robert/configuration/H2ConsoleConfig.java
+++ src/main/java/robert/configuration/H2ConsoleConfig.java
@@ -1,15 +1,20 @@
 package robert.configuration;
 
+import org.h2.server.web.WebServlet;
+import org.springframework.boot.context.embedded.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 /**
  * Created by robert on 09.04.16.
  */
-//@Configuration
+@Configuration
 public class H2ConsoleConfig {
 
-   /* @Bean
+    @Bean
     ServletRegistrationBean h2servletRegistration() {
         ServletRegistrationBean registrationBean = new ServletRegistrationBean(new WebServlet());
         registrationBean.addUrlMappings("/console*//*");
         return registrationBean;
-    }*/
+    }
 }
