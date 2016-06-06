b98f1cde599981dee80579f1988dbd82e05d8ab3
robert <nowak021@gmail.com>
2016-06-06 17:51:41 +0200

    todo: aspect methods for checking and generating UUID

--- src/main/java/robert/InvoiceWriterApplication.java
+++ src/main/java/robert/InvoiceWriterApplication.java
@@ -2,12 +2,14 @@ package robert;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableJpaRepositories
 @EnableTransactionManagement
+@EnableAspectJAutoProxy
 public class InvoiceWriterApplication {
     public static void main(String[] args) {
         SpringApplication.run(InvoiceWriterApplication.class, args);
--- src/main/java/robert/aop/ControllerAspect.java
+++ src/main/java/robert/aop/ControllerAspect.java
@@ -3,14 +3,16 @@ package robert.aop;
 import org.apache.log4j.Logger;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import robert.services.api.FactoryUUID;
 
 /**
  * Created by robert on 06.06.16.
  */
 @Aspect
-public class ControllerAdvice {
-    private static final Logger logger = Logger.getLogger(ControllerAdvice.class);
+@Component
+public class ControllerAspect {
+    private static final Logger logger = Logger.getLogger(ControllerAspect.class);
 
     @Autowired
     private FactoryUUID factoryUUID;
